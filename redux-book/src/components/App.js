96df597e039d910610a783d511636450457dfe64
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-12 16:34:48 -0700

    Created book detail container and import BookDetail to app.js

--- redux-book/src/components/App.js
+++ redux-book/src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from '../assets/images/logo.svg';
 import '../assets/css/App.css';
 import BookList from '../containers/book_list';
+import BookDetail from '../containers/book-detail';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BookList />
+        <BookDetail />
       </div>
     );
   }
