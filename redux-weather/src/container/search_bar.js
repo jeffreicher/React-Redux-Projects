e476c3b7b0750e8f17a830c499a82f023c319371
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-13 16:47:29 -0700

    Added event handler

--- redux-weather/src/container/search_bar.js
+++ redux-weather/src/container/search_bar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+    };
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    };
+
     render() {
         return (
             <form className="input-group">
-            <input />
-            <span className="input-group-btn">
-                <button type="submit" className="btn btn-secondary">Submit</button>
-            </span>
+                <input placeholder="Get a fave-day forecast in your favorite cities" className="form-control" value={this.state.term} onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
             </form>
         );
     };
