f58dacafa5d02642ea5ee476f982668da8e4800e
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-13 16:02:12 -0700

    installed redux and react-redux as well as updated index.js file

--- redux-weather/src/index.js
+++ redux-weather/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './components/App';
 import registerServiceWorker from './helpers/registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+<App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
