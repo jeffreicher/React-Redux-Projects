53d3cb9e93d2c0bb6dc577f7b1cb0c25aef0cec3
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-15 14:05:08 -0700

    Set up api request and call as well as installed axios

--- redux-weather/src/actions/index.js
+++ redux-weather/src/actions/index.js
@@ -1,1 +1,10 @@
 const API_KEY = 'e3f32ab3b05bd56f2be3b430f0e62d9a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    };
+};
