c8b3629c1389c1c627fe7934a75815e25626723c
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-18 15:34:38 -0700

    imported fetch weather type and created reducer for fetching weather

--- redux-weather/src/reducers/reducer_weather.js
+++ redux-weather/src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-    console.log('Action recieved', action);
-    return state;
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+            // return state.concat([ action.payload.data ]); same as below
+            return [ action.payload.data, ...state ];
+        default:
+            return state;
+    }
 }
