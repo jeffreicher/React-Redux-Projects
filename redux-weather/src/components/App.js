bf49245510ae3df22e89a59cb855da6eea4629ac
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-18 15:37:43 -0700

    created skeleton for weather list container and imported into app.js

--- redux-weather/src/components/App.js
+++ redux-weather/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../container/search_bar';
+import WeatherList from '../container/weather_list';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <SearchBar />
+        <WeatherList />
       </div>      
     );
   };
