f9200c04910985771bfac52573066b3bb5de4b92
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-18 16:22:29 -0700

    refactored charts into seperate component and imported to weather list

--- redux-weather/src/container/weather_list.js
+++ redux-weather/src/container/weather_list.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData, key) {
         const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
+
         return (
             <tr key={ key }>
                 <td>{ cityData.city.name }</td>
                 <td>
-                    <Sparklines height={ 120 } width={ 180 } data={ temps }>
-                        <SparklinesLine color="red" />
-                    </Sparklines>
+                    <Chart data={ temps } color="red" />
                 </td>
             </tr>
         );
