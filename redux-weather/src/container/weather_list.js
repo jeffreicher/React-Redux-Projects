770c2009c302f2015096c82d9d27be846adfe22e
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-18 16:42:29 -0700

    Added lodash and created avg using helper functions as well as added avg line

--- redux-weather/src/container/weather_list.js
+++ redux-weather/src/container/weather_list.js
@@ -5,12 +5,20 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
     renderWeather(cityData, key) {
         const temps = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={ key }>
                 <td>{ cityData.city.name }</td>
                 <td>
                     <Chart data={ temps } color="red" />
+                </td>
+                <td>
+                    <Chart data={ pressure } color="green" />  
+                </td>
+                <td>
+                    <Chart data={ humidity } color="black" />
                 </td>
             </tr>
         );
