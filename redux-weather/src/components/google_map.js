4c6f79cdf7c5a809afae0fb119d777b51c77393f
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-18 17:59:19 -0700

    removed unused property from google maps

--- redux-weather/src/components/google_map.js
+++ redux-weather/src/components/google_map.js
@@ -4,14 +4,14 @@ import '../assets/css/styles.css';
 const google = window.google;
 
 class GoogleMap extends Component {
+    
     componentDidMount() {
         new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
                 lng: this.props.lon
-            },
-            innerHeight: 500
+            }
         });
     };
 
