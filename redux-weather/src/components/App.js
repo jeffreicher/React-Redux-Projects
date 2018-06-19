47052566d22c147b7ce1f75d30bf4f2f6ec71cee
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-19 16:59:28 -0700

    imported header into app file

--- redux-weather/src/components/App.js
+++ redux-weather/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from '../container/search_bar';
 import WeatherList from '../container/weather_list';
+import Header from './header';
 import '../assets/css/styles.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Header />
         <SearchBar />
         <WeatherList />
       </div>      
