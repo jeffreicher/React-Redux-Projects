fd51c6bd7225d85ebc56ff8a886b72f8a30cdde1
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-21 14:22:29 -0700

    Added react router and set up file for post app

---
+++ redux-posts/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Header from './header';
+import '../assets/css/styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+
+        </div>         
+      </div>
+     
+    );
+  };
+};
+
+export default App;
