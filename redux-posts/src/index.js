c0a4d7280ddda7c48f04dae0629104a2736eaeb8
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-21 14:23:54 -0700

    Fixed multiple children elements inside Router

--- redux-posts/src/index.js
+++ redux-posts/src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './helpers/registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import reducers from './reducers';
+import reducers from './reducers';
 // import ReduxPromise from 'redux-promise';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -22,8 +22,10 @@ class Goodbye extends React.Component {
 ReactDOM.render(
 <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <div>
+            <Route path="/hello" component={Hello} />
+            <Route path="/goodbye" component={Goodbye} />            
+        </div>
     </Router>
 </Provider>
 , document.getElementById('root'));
