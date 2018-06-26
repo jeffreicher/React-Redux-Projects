6ee6ec3ef8a450808314766eef21156671d846e9
Jeffrei Cher <jeffreicher@yahoo.com>
2018-06-26 11:13:06 -0700

    cleaned up files in index.js and routed home page to PostIndex

--- redux-posts/src/index.js
+++ redux-posts/src/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import registerServiceWorker from './helpers/registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import reducers from './reducers';
+import PostIndex from './components/posts_index';
 // import ReduxPromise from 'redux-promise';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-    render() { return <div>Hello</div> }
-}
-
-class Goodbye extends React.Component {
-    render() { return <div>Goodbye</div> }
-}
-
 ReactDOM.render(
 <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
         <div>
-            <Route path="/hello" component={Hello} />
-            <Route path="/goodbye" component={Goodbye} />            
+            <Route path="/" component={PostIndex} />
         </div>
     </Router>
 </Provider>
