6b307af7b781ab3e01ee8321b92783162b56e369
Jeffrei Cher <jeffreicher@yahoo.com>
2018-07-03 13:08:46 -0700

    added helper function to display field

--- redux-posts/src/components/posts_new.js
+++ redux-posts/src/components/posts_new.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div>
+                <input type="text" {...field.input} />
+            </div>
+        )
+    }
     render() {
         return (
             <form action="">
-                <Field name="title"  component={} />
+                <Field name="title"  component={this.renderTitleField} />
             </form>
         );
     };
